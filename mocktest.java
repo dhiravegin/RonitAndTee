820be2d3014c1c00a45532f87c544ae825503b95
dhiravegin <70318195+dhiravegin@users.noreply.github.com>
2020-10-08 19:45:53 +0800

    Add files via upload

---
+++ mocktest.java
@@ -0,0 +1,46 @@
+
+/*
+ * mocktest#.java
+ * 
+ * Copyright 2020 Unknown <ace@T460>
+ * 
+ * This program is free software; you can redistribute it and/or modifyFITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or 
+ */
+
+
+public class mocktest
+{
+	
+	public static void main (String[] args) 
+	{  
+		System.out.println("My name is Tee");
+		int base = IBIO.inputInt("Base = ");
+		int exponent = IBIO.inputInt("Exponent = ");
+		int power =1;
+		for(int i = 0; i < exponent; i++)
+		{	
+		//int n= IBIO.inputInt(" Enter an integer:");
+		if( base == 0 ) 
+		{
+			IBIO.output("Base = zero. Nothing to do"); 
+			System.exit(0);
+		}
+		}
+
+	}
+}
